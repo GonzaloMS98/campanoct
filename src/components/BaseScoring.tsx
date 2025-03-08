bdef0ff242fb91beb9814b1b53720fcbe939e148
GonzaloMS98 <gonzalomonsa94@gmail.com>
2025-03-07 22:03:03 -0700

    email update

--- src/components/BaseScoring.tsx
+++ src/components/BaseScoring.tsx
@@ -136,8 +136,6 @@ export default function BaseScoring() {
                 <input
                   type="number"
                   id="points"
-                  min="0"
-                  max="10"
                   value={points}
                   onChange={handlePointsChange}
                   className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
